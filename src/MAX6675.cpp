f06b60373247928c0e68c95c953c923c5742504d
enjoyneering <enjoyneering79@gmail.com>
2017-10-28 21:02:40 -0400

    Update MAX6675.cpp

--- src/MAX6675.cpp
+++ src/MAX6675.cpp
@@ -34,16 +34,16 @@
 /**************************************************************************/
 MAX6675::MAX6675(uint8_t cs, uint8_t so, uint8_t sck)
 {
-  _cs    = cs;
-  _so    = so;
-  _sck   = sck;
+  _cs  = cs;
+  _so  = so;
+  _sck = sck;
 }
 
 /**************************************************************************/
 /*
     begin()
 
-    Initializes & configures I2C
+    Initializes & configures gpio for serial bit-bang interface 
 */
 /**************************************************************************/
 void MAX6675::begin(void)
@@ -94,12 +94,12 @@ uint16_t MAX6675::getChipID(uint16_t rawValue)
 /*
     getTemperature()
 
-    Reads Temperature, C
+    Reads Temperature, °C
 
     NOTE:
     - bits D14..D3 contain the converted temperature in the order of MSB to LSB.
-    - sequence of all zeros on SO pin means the thermocouple reading is 0C
-      sequence of all ones  on SO pin means the thermocouple reading is +1023.75C
+    - sequence of all zeros on SO pin means the thermocouple reading is 0°C
+    - sequence of all ones  on SO pin means the thermocouple reading is +1023.75°C
 */
 /**************************************************************************/
 float MAX6675::getTemperature(uint16_t rawValue)
@@ -128,8 +128,8 @@ float MAX6675::getTemperature(uint16_t rawValue)
       to allow the operation of the open thermocouple detector the "T-" pin must be grounded
     - bit D1 is low to provide a device ID for the MAX6675
     - bit D0 is three-state
-    - sequence of all zeros on SO pin means the thermocouple reading is 0C
-      sequence of all ones  on SO pin means the thermocouple reading is +1023.75C
+    - sequence of all zeros on SO pin means the thermocouple reading is 0°C
+    - sequence of all ones  on SO pin means the thermocouple reading is +1023.75°C
 */
 /**************************************************************************/
 uint16_t MAX6675::readRawData(void)
