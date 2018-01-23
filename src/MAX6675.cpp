acd8f96cbcba8c9808f18c6480a97d27cc6977de
enjoyneering <enjoyneering79@gmail.com>
2018-01-22 20:21:08 -0500

    Update MAX6675.cpp

--- src/MAX6675.cpp
+++ src/MAX6675.cpp
@@ -13,7 +13,17 @@
    written by : enjoyneering79
    sourse code: https://github.com/enjoyneering/MAX6675
 
-   BSD license, all text above must be included in any redistribution
+   This sensor can work with hardware SPI bus, specials pins are required to interface
+
+   Connect chip to pins:    SCLK        MOSI        don't use SS for CS  don't use MISO for CS
+   Uno, Mini, Pro:          13          12          10                   11
+   Mega2560, Due:           52          51          53                   50
+   Leonardo, ProMicro:      15          16          x                    14
+   NodeMCU 1.0:             GPIO14/D5   GPIO13/D7   GPIO15/D8            GPIO12/D6
+   WeMos D1 Mini:           GPIO14/D5   GPIO13/D7   GPIO15/D8            GPIO12/D6 
+
+  GNU GPL license, all text above must be included in any redistribution, see link below for details:
+  - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
 
@@ -24,7 +34,7 @@
 /*
     MAX6675()
 
-    Constructor
+    Constructor for software/bit-bang serial interface
 
     NOTE:
     cs  - chip select, set CS low to enable the serial interface
@@ -34,26 +44,59 @@
 /**************************************************************************/
 MAX6675::MAX6675(uint8_t cs, uint8_t so, uint8_t sck)
 {
-  _cs  = cs;
-  _so  = so;
-  _sck = sck;
+  _useHardwareSPI = false; //false ->sw spi
+
+  _cs  = cs;               //sw ss
+  _so  = so;               //sw miso
+  _sck = sck;              //sw sclk
+}
+
+/**************************************************************************/
+/*
+    MAX6675()
+
+    Constructor for hardware SPI serial interface
+
+    NOTE:
+    - chip select "cs", set CS low to enable the serial interface
+*/
+/**************************************************************************/
+MAX6675::MAX6675(uint8_t cs)
+{
+  _useHardwareSPI = true; //true -> hw spi
+
+  _cs  = cs;              //hw ss
+  _so  = 0;               //sw miso
+  _sck = 0;               //sw sclk
 }
 
 /**************************************************************************/
 /*
     begin()
 
-    Initializes & configures gpio for serial bit-bang interface 
+    Initializes & configures gpio for bit-bang or hardware serial interface
+
+    NOTE:
+
 */
 /**************************************************************************/
 void MAX6675::begin(void)
 {
   pinMode(_cs, OUTPUT);
-  pinMode(_so, INPUT);
-  pinMode(_sck, OUTPUT);
+  digitalWrite(_cs, HIGH);     //disables the serial interface, but it will initiate measurement/conversion
 
-  digitalWrite(_cs, HIGH); //disables the serial interface & start new measurement process
-  digitalWrite(_sck, LOW);
+  switch (_useHardwareSPI)     //true -> hw spi, false ->sw spi
+  {
+    case true:
+      SPI.begin();             //setting hardware SCK, MOSI, SS to output, pull SCK, MOSI low & SS high      
+      break;
+
+    case false:
+      pinMode(_so, INPUT);
+      pinMode(_sck, OUTPUT);
+      digitalWrite(_sck, LOW);     
+      break;
+  }
 }
 
 /**************************************************************************/
@@ -86,7 +129,10 @@ bool MAX6675::detectThermocouple(uint16_t rawValue)
 /**************************************************************************/
 uint16_t MAX6675::getChipID(uint16_t rawValue)
 {
-  if (bitRead(rawValue, 1) == 0) return 6675;
+  if (rawValue == MAX6675_FORCE_READ_DATA) rawValue = readRawData();
+
+  if (bitRead(rawValue, 1) == 0) return MAX6675_ID;
+
   return 0;
 }
 
@@ -104,7 +150,9 @@ uint16_t MAX6675::getChipID(uint16_t rawValue)
 /**************************************************************************/
 float MAX6675::getTemperature(uint16_t rawValue)
 {
-  if (detectThermocouple(rawValue) != true || getChipID(rawValue) != 6675) return MAX6675_ERROR;
+  if (rawValue == MAX6675_FORCE_READ_DATA) rawValue = readRawData();
+
+  if (detectThermocouple(rawValue) != true || getChipID(rawValue) != MAX6675_ID) return MAX6675_ERROR;
 
   rawValue = rawValue >> 3; //clear D2..D0 bits, rawValue - 0..4095
 
@@ -130,29 +178,43 @@ float MAX6675::getTemperature(uint16_t rawValue)
     - bit D0 is three-state
     - sequence of all zeros on SO pin means the thermocouple reading is 0°C
     - sequence of all ones  on SO pin means the thermocouple reading is +1023.75°C
+
+    - max SPI master clock speed is equal with board speed
+      (16000000UL for 5V 16MHz/ProMini), but MAX6675 max speed is only 4.3MHz
+    - SPI_MODE0 -> capture data on clock's falling edge.
 */
 /**************************************************************************/
 uint16_t MAX6675::readRawData(void)
 {
   uint16_t rawData = 0;
 
-  /* stops conversion & initiate a new measurement process */
-  digitalWrite(_cs, LOW);         //stop  conversion
-  digitalWrite(_cs, HIGH);        //start conversion
+  digitalWrite(_cs, LOW);                                                //stop  measurement/conversion
+  delay(1);                                                              //4MHz  is 0.25usec, do we need it???
+  digitalWrite(_cs, HIGH);                                               //start measurement/conversion
   delay(MAX6675_CONVERSION_TIME);
 
-  /* set CS low to enable serial interface, read 16-bits, in order MSB->LSB (15..0bit) */
-  digitalWrite(_cs, LOW);
+  digitalWrite(_cs, LOW);                                                //set CS low to enable serial interface
 
-  for (int8_t i = 16; i >= 0; i--)
+  switch (_useHardwareSPI)                                               //true -> hw spi, false ->sw spi
   {
-    digitalWrite(_sck, HIGH);
-    rawData = (rawData << 1) | digitalRead(_so);
-    digitalWrite(_sck, LOW);
-  }
+    case true:
+      SPI.beginTransaction(SPISettings(4000000UL, MSBFIRST, SPI_MODE0)); //speed 4MHz, read msb first, spi mode 0, see note
+      rawData = SPI.transfer16(0x0000);                                  //MAX6675 has read only spi & mosi not connected, doesn't metter what to send
+      break;
 
-  /* disables the serial interface & initiate measurement */
-  digitalWrite(_cs, HIGH);
+    case false:
+      for (uint8_t i = 0; i < 16; i++)                                   //read 16-bits via software spi, in order MSB->LSB (D15..D0 bit)
+      {
+        digitalWrite(_sck, HIGH);
+        rawData = (rawData << 1) | digitalRead(_so);
+        digitalWrite(_sck, LOW);
+      }
+      break;
+  }  
+
+  digitalWrite(_cs, HIGH);                                               //disables the serial interface, but it will initiate measurement/conversion
+
+  if (_useHardwareSPI == true) SPI.endTransaction();                     //call to de-asserting hw chip select & free hw spi for other slaves
 
   return rawData;
 }
