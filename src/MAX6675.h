31e4c302481926f0f8d93e8035f573501356a226
enjoyneering <enjoyneering79@gmail.com>
2018-01-22 20:20:22 -0500

    Update MAX6675.h

--- src/MAX6675.h
+++ src/MAX6675.h
@@ -13,7 +13,17 @@
    written by : enjoyneering79
    sourse code: https://github.com/enjoyneering/MAX6675
 
-   BSD license, all text above must be included in any redistribution
+   This sensor can work with hardware SPI bus, specials pins are required to interface
+
+   Connect chip to pins:    SCLK        MOSI        don't use SS for CS  don't use MISO for CS
+   Uno, Mini, Pro:          13          12          10                   11
+   Mega2560, Due:           52          51          53                   50
+   Leonardo, ProMicro:      15          16          x                    14
+   NodeMCU 1.0:             GPIO14/D5   GPIO13/D7   GPIO15/D8            GPIO12/D6
+   WeMos D1 Mini:           GPIO14/D5   GPIO13/D7   GPIO15/D8            GPIO12/D6 
+
+  GNU GPL license, all text above must be included in any redistribution, see link below for details:
+  - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
 
@@ -25,6 +35,7 @@
 #else
 #include <WProgram.h>
 #endif
+#include <SPI.h>
 
 #if defined (__AVR__)
 #include <avr/pgmspace.h>
@@ -32,23 +43,28 @@
 #include <pgmspace.h>
 #endif
 
-#define MAX6675_CONVERSION_TIME 220  //milliseconds or sampling rate ~4...5Hz
-#define MAX6675_RESOLUTION      0.25 //deg.C per dac step
-#define MAX6675_ERROR          -255  //if any error
+#define MAX6675_CONVERSION_TIME 220    //in milliseconds, sampling rate ~4...5Hz
+#define MAX6675_RESOLUTION      0.25   //in deg.C per dac step
+
+#define MAX6675_ID              6675
+#define MAX6675_FORCE_READ_DATA 0x8002 //force to read the data, is unique because d15d1 can't be high
+#define MAX6675_ERROR           2000   //returned value if any error happends
 
 class MAX6675
 {
   public:
    MAX6675(uint8_t cs, uint8_t so, uint8_t sck);
+   MAX6675(uint8_t cs);
 
    void     begin(void);
-   bool     detectThermocouple(uint16_t rawValue);
-   uint16_t getChipID(uint16_t rawValue);
-   float    getTemperature(uint16_t rawValue);
+   bool     detectThermocouple(uint16_t rawValue = MAX6675_FORCE_READ_DATA);
+   uint16_t getChipID(uint16_t rawValue = MAX6675_FORCE_READ_DATA);
+   float    getTemperature(uint16_t rawValue = MAX6675_FORCE_READ_DATA);
    uint16_t readRawData(void);
  
 
   private:
+   bool    _useHardwareSPI;
    uint8_t _cs;
    uint8_t _so;
    uint8_t _sck;
