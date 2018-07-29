73318fcc9e650569641194d53030c43fcd16780b
enjoyneering <enjoyneering79@gmail.com>
2018-07-29 14:15:05 -0400

    Update MAX6675.h

--- src/MAX6675.h
+++ src/MAX6675.h
@@ -1,26 +1,34 @@
 /***************************************************************************************************/
 /*
    This is an Arduino library for 12-bit MAX6675 K-Thermocouple to Digital Converter
-   with Cold Junction Compensation & maximum sampling rate ~4...5Hz
+   with Cold Junction Compensation conneted to hardware 4Mhz SPI with maximum sampling
+   rate ~4..5Hz.
 
+   - MAX6675 power supply voltage is 3.0 - 5.5v
    - K-type thermocouples have an absolute accuracy of around ±2°C
    - Measurement tempereture range 0°C...+1024°C with 0.25°C resolution
    - Cold junction compensation range -20°C...+85°C
    - Keep K-type thermocouple cold junction & MAX6675 at the same temperature
    - Avoid placing heat-generating devices or components near the converter
      because this may produce errors
+   - It is strongly recommended to add a 10nF/0.01mF ceramic surface-mount capacitor, placed across
+     the T+ and T- pins, to filter noise on the thermocouple lines.
      
    written by : enjoyneering79
    sourse code: https://github.com/enjoyneering/MAX6675
 
-   This sensor can work with hardware SPI bus, specials pins are required to interface
+   This sensor uses SPI bus to communicate, specials pins are required to interface
+   Board:                                    MOSI        MISO        SCLK         SS, don't use for CS   Level
+   Uno, Mini, Pro, ATmega168, ATmega328..... 11          12          13           10                     5v
+   Mega, Mega2560, ATmega1280, ATmega2560... 51          50          52           53                     5v
+   Due, SAM3X8E............................. ICSP4       ICSP1       ICSP3        x                      3.3v
+   Leonardo, ProMicro, ATmega32U4........... 16          14          15           x                      5v
+   Blue Pill, STM32F103xxxx boards.......... PA17        PA6         PA5          PA4                    3v
+   NodeMCU 1.0, WeMos D1 Mini............... GPIO13/D7   GPIO12/D6   GPIO14/D5    GPIO15/D8*             3v/5v
+   ESP32.................................... GPIO23/D23  GPIO19/D19  GPIO18/D18   x                      3v
 
-   Connect chip to pins:    SCLK        MOSI        don't use SS for CS  don't use MISO for CS
-   Uno, Mini, Pro:          13          12          10                   11
-   Mega2560, Due:           52          51          53                   50
-   Leonardo, ProMicro:      15          16          x                    14
-   NodeMCU 1.0:             GPIO14/D5   GPIO13/D7   GPIO15/D8            GPIO12/D6
-   WeMos D1 Mini:           GPIO14/D5   GPIO13/D7   GPIO15/D8            GPIO12/D6 
+                                            *if GPIO2/D4 or GPIO0/D3 used for for CS, apply an external 25kOhm
+                                             pullup-down resistor
 
   GNU GPL license, all text above must be included in any redistribution, see link below for details:
   - https://www.gnu.org/licenses/licenses.html
@@ -30,17 +38,22 @@
 #ifndef MAX6675_h
 #define MAX6675_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
+#if defined(ARDUINO) && ARDUINO >= 100 //arduino core v1.0 or later
 #include <Arduino.h>
 #else
 #include <WProgram.h>
 #endif
-#include <SPI.h>
 
-#if defined (__AVR__)
-#include <avr/pgmspace.h>
+#if defined(__AVR__)
+#include <avr/pgmspace.h>              //use for PROGMEM Arduino AVR
 #elif defined(ESP8266)
-#include <pgmspace.h>
+#include <pgmspace.h>                  //use for PROGMEM Arduino ESP8266
+#elif defined(_VARIANT_ARDUINO_STM32_)
+#include <avr/pgmspace.h>              //use for PROGMEM Arduino STM32
+#endif
+
+#ifndef  MAX6675_SOFT_SPI              //enable upload spi.h
+#include <SPI.h>
 #endif
 
 #define MAX6675_CONVERSION_TIME 220    //in milliseconds, sampling rate ~4...5Hz
@@ -53,7 +66,6 @@
 class MAX6675
 {
   public:
-   MAX6675(uint8_t cs, uint8_t so, uint8_t sck);
    MAX6675(uint8_t cs);
 
    void     begin(void);
@@ -64,10 +76,9 @@ class MAX6675
  
 
   private:
-   bool    _useHardwareSPI;
+
+  protected:
    uint8_t _cs;
-   uint8_t _so;
-   uint8_t _sck;
 };
 
 #endif
